7d0fa7fc0aba9f6b7166f5937898ef0a0a18c09d
HTDPNJ <TOliverQueen1@163.com>
2019-03-12 20:58:31 +0800

    commit

--- src/Test.java
+++ src/Test.java
@@ -1,45 +1,40 @@
-import java.math.BigInteger;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
 
 public class Test
 {
     public static void main(String[] args)
     {
             int[] data={5,4,9,7,6,1,8,3,10,2};
-            quicksort(data,0,data.length-1);
+            int ans=quicksort(data,0,data.length-1);
         System.out.println(Arrays.toString(data));
     }
-    public static void quicksort(int[]arr,int l,int r){
+    public static int quicksort(int[]arr,int l,int r){
         if(l<r){
             int q=partion(arr,l,r);
             quicksort(arr,l,q-1);
             quicksort(arr,q+1,r);
         }
+        return 0;
     }
     public static int  partion(int []arr,int l,int r){
-        int parpoi=l;
-        int tem=arr[parpoi];
+        int key=arr[l];
         while (l<r){
-            while(l<r&&arr[r]>=tem){
+            while(l<r&&arr[r]>=key){
                 r--;
             }
             if(l<r){
-                arr[parpoi]=arr[r];
-                parpoi=r;
+                arr[l]=arr[r];
                 l++;
             }
-            while(l<r&&arr[l]<=tem){
+            while(l<r&&arr[l]<=key){
                 l++;
             }
             if(l<r){
-                arr[parpoi]=arr[l];
-                parpoi=l;
+                arr[r]=arr[l];
                 r--;
             }
         }
-        arr[parpoi]=tem;
-        return parpoi;
+        arr[l]=key;
+        return l;
     }
 }
